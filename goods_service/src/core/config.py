2c56ceeac1a44e4509cb3a56c32639351f3927ed
Ivan Svyatykh <zlativansvyat@mail.ru>
2025-06-24 14:20:13 +0500

    Более правильно настроил DI

--- goods_service/src/core/config.py
+++ goods_service/src/core/config.py
@@ -2,6 +2,13 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+# Database
 DATABASE_URL = os.environ.get("DATABASE_URL")
+# Service envs
 PORT = int(os.environ.get("PORT"))
+# MinIO
+MINIO_API_PORT = os.environ.get("MINIO_API_PORT")
+MINIO_HOST = os.environ.get("MINIO_HOST")
+MINIO_SECRET_KEY = os.environ.get("MINIO_SECRET_KEY")
+MINIO_ACCESS_KEY = os.environ.get("MINIO_ACCESS_KEY")
+USE_HTTPS = bool(os.environ.get("USE_HTTPS"))
--- goods_service/src/core/main.py
+++ goods_service/src/core/main.py
@@ -1,11 +1,37 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
+from miniopy_async import Minio
+
 from goods_service.src.presentation.rest.v1.goods import goods_router
 from config import PORT
 from goods_service.src.domain.exceptions.goods_exceptions import DomainException
 from goods_service.src.presentation.rest.error_handlers.goods_handlers import domain_error_handler
+from goods_service.src.core.config import (MINIO_HOST, MINIO_SECRET_KEY, MINIO_ACCESS_KEY, MINIO_API_PORT, USE_HTTPS)
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import async_sessionmaker
 
-app = FastAPI()
+from goods_service.src.core.config import DATABASE_URL
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.minio_client = Minio(endpoint=f"{MINIO_HOST}:{MINIO_API_PORT}",
+                                   access_key=MINIO_ACCESS_KEY,
+                                   secret_key=MINIO_SECRET_KEY,
+                                   secure=USE_HTTPS)
+
+    engine = create_async_engine(DATABASE_URL, pool_size=10,
+                                 max_overflow=20, )
+
+    app.state.async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+    yield
+    await app.state.minio_client.close_session()
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(goods_router)
 app.exception_handler(DomainException)(domain_error_handler)
