953a51c607621d8bb92606bfcf91f5fd07737b2a
Ivan Svyatykh <zlativansvyat@mail.ru>
2025-06-26 14:35:31 +0500

    заменил тип цены

--- goods_service/src/infrastructure/db/models/model.py
+++ goods_service/src/infrastructure/db/models/model.py
@@ -3,6 +3,7 @@ import uuid
 from sqlalchemy import (
     Column,
     String,
+    Float,
     Integer, ForeignKey,
 )
 from sqlalchemy.dialects.postgresql import UUID, MONEY
@@ -13,25 +14,25 @@ Base = declarative_base()
 
 class GoodCategoryModel(Base):
     __tablename__ = "good_category"
-    __table_args__ = {"schema": "goods"}
+    __table_args__ = {"schema": "public"}
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = Column(String(254), nullable=False, unique=True)
 
 
 class GoodsModel(Base):
     __tablename__ = "goods"
-    __table_args__ = {"schema": "goods"}
+    __table_args__ = {"schema": "public"}
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    category_id = Column(UUID(as_uuid=True), ForeignKey("goods.good_category.id"), nullable=False)
+    category_id = Column(UUID(as_uuid=True), ForeignKey("public.good_category.id"), nullable=False)
     name = Column(String, nullable=False, unique=True)
-    price = Column(MONEY, nullable=True)
+    price = Column(Float(precision=2), nullable=False, default=0.0, )
     amount = Column(Integer, nullable=False, default=0)
 
 
 class GoodsPhotosModel(Base):
     __tablename__ = "goods_photos"
-    __table_args__ = {"schema": "goods"}
+    __table_args__ = {"schema": "public"}
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     photo_url = Column(String, nullable=False, unique=True)
-    good_id = Column(UUID(as_uuid=True), ForeignKey("goods.goods.id"), nullable=False)
+    good_id = Column(UUID(as_uuid=True), ForeignKey("public.goods.id"), nullable=False)
     description = Column(String, nullable=True)
