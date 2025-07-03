659cb374333266ab092981db3ce929068ce921f7
Ivan Svyatykh <zlativansvyat@mail.ru>
2025-07-03 18:57:03 +0500

    сгенерировал файлы для grpc

--- auth_service/src/presentation/grpc/generated/auth_pb2.pyi
+++ auth_service/src/presentation/grpc/generated/auth_pb2.pyi
@@ -1,7 +1,7 @@
-from google.protobuf import empty_pb2 as _empty_pb2
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar as _ClassVar, Optional as _Optional
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
